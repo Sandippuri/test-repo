f587707eaccb714356b704bd134ff5a227cddc8c
itsPuri <sandip.puri980@gmail.com>
2024-02-04 13:36:44 +0545

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,19 @@
 // import Sorting from "./dsa/sorting";
 // import SortingVisualizer from "./dsa/sorting/sort";
 // import Valentine from "./willyoube/Valentine";
-// import Valentine2 from "./willyoube/valentine2";
+import Valentine2 from "./willyoube/valentine2";
 
-import DateMe from "./components/dateme/DateMe";
+// import DateMe from "./components/dateme/DateMe";
 
 
 function App() {
   return (
     <div className="bg-[#FAFBF8] w-screen h-screen">
       {/* <Collapsable/> */}
-      <DateMe/>
+      {/* <DateMe/> */}
       {/* <CosmWasm/> */}
       {/* <ObjectGroupby/> */}
-      {/* <Valentine2/> */}
+      <Valentine2/>
       {/* <Sorting/> */}
       {/* <SortingVisualizer/> */}
      {/* <MoveAwayButton/> */}
